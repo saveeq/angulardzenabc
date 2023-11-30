f60af338913ff360a259beaaaefe70cb910d84b2
5a ve eq! <saveeeqx@gmail.com>
2023-11-30 09:24:38 +0300

    third

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
+import {AppComponent} from "./app.component";
+import {CategoriesComponent} from "./views/categories/categories.component";
 
 import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
---
+++ src/app/views/categories/categories.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.css'
+})
+export class CategoriesComponent {
+
+  ngOnInit() {
+
+  }
+}
